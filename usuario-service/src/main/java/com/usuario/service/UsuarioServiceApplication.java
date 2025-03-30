23c6ee18d6da469c8db638b2ddbf6c8c1edcdab7
robim16 <luis.hoyos@dydsoluciones.net>
2025-03-30 15:47:03 -0500

    creacion del config server

--- usuario-service/src/main/java/com/usuario/service/UsuarioServiceApplication.java
+++ usuario-service/src/main/java/com/usuario/service/UsuarioServiceApplication.java
@@ -2,10 +2,11 @@ package com.usuario.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class UsuarioServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UsuarioServiceApplication.class, args);
 	}
