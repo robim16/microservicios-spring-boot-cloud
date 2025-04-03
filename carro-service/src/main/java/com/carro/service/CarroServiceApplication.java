ea996429871b472810203f5d94e05802a4bf3d26
robim16 <luis.hoyos@dydsoluciones.net>
2025-04-03 16:22:00 -0500

    instalación de eureka client

--- carro-service/src/main/java/com/carro/service/CarroServiceApplication.java
+++ carro-service/src/main/java/com/carro/service/CarroServiceApplication.java
@@ -3,6 +3,8 @@ package com.carro.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@EnableEurekaClient
 @SpringBootApplication
 public class CarroServiceApplication {
 
