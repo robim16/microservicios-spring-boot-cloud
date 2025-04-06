07b41b6527341db0678cd1f47af2dbb17fc12ca9
robim16 <luis.hoyos@dydsoluciones.net>
2025-04-06 15:50:02 -0500

    configurar microservicios multiples instancias

--- carro-service/src/main/java/com/carro/service/CarroServiceApplication.java
+++ carro-service/src/main/java/com/carro/service/CarroServiceApplication.java
@@ -3,8 +3,6 @@ package com.carro.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-@EnableEurekaClient
 @SpringBootApplication
 public class CarroServiceApplication {
 
--- usuario-service/src/main/java/com/usuario/service/UsuarioServiceApplication.java
+++ usuario-service/src/main/java/com/usuario/service/UsuarioServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableEurekaClient
 public class UsuarioServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UsuarioServiceApplication.class, args);
